230e23fcce4fc1138d82dccb4473560c3e9d5b3e
Adam Rogalski <adamcio.rogalski1@gmail.com>
2022-07-26 08:18:58 +0200

    Update RabbitMqConfig.java

--- src/main/java/com/example/lbdkolejki/Configuration/RabbitMqConfig.java
+++ src/main/java/com/example/lbdkolejki/Configuration/RabbitMqConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-public class RabbitMqTopicConfig {
+public class RabbitMqConfig {
     @Bean
     Queue articleQueue(){
         return new Queue("articleQueue",false);
